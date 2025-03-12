fa480e396383614eddd2aaf5063bd42d9a6fdd0f
Lepak2009 <maryanlepak122@gmail.com>
2025-03-12 18:18:47 +0200

    init

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello ")
+import asyncio
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+          
